845fd7982ab5f49a9af2bcfcf29ed7a4aa6bd0ea
Natnael-Negash <nnegash658@gmail.com>
2024-03-23 07:53:06 -0700

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file {'etc/ssh/ssh_cofig':
+       ensure => present,
+content =>"
+
+  #SSH client configuration
+  host*
+  IdentityFile ~/.ssh/school
+  PasswordAuthenthication no
+}
