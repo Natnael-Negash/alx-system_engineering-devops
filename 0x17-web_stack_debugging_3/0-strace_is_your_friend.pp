56e72210375ffa5e35b77242521f86550218b009
Natnael-Negash <nnegash658@gmail.com>
2024-06-23 09:08:01 -0900

    web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes Apache 500 error by correcting a typo in WordPress configuration file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
