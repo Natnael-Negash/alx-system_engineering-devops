9cc838c416959263038957233b61eaac24016d22
Natnael-Negash <nnegash658@gmail.com>
2024-06-27 04:33:41 -0900

    web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
-# Increase hard file limit for the user 'holberton'
-exec { 'increase-hard-file-limit-for-holberton-user':
+# Reconfigure the OS for 'holberton' to login and open a file without any error message
+
+exec { 'increase-hard-file-limit-holberton-user':
   command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for the user 'holberton'
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+
